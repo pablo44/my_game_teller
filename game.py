21351ece31ec9fca8b0a7a40db0125a51685e62d
pablo44 <pawelrz@wp.pl>
2023-09-25 14:02:56 +0200

    welcome and options parts

--- game.py
+++ game.py
@@ -1,2 +1,18 @@
 #WELCOME 
 print('Welcome to the "CASTLE GAME"')
+print('********************************')
+print('You are in the Town of Abarka \n The Town is surrounded by the mountains and placed by the river')
+name = input('Would You please introduce Yourselve, Write Your Name: ')
+print(f'So {name} You are standing at the marketplace now in the center of the Town of Abarka')
+#options
+abarka = ['north', 'west', 'east','south']
+print(f'You have \n on Your {abarka[0]} side Mountains \n on Your {abarka[1]} side Valley \n on Your {abarka[2]} side Desert \n on Your {abarka[3]} side River')
+"*************************************"
+userTurn = ""
+'*********************'
+userTurn= input
+print('choose your number: \n 1 north \n 2 west \n 3 east \n 4 south')
+print(input(f'{name} choose where do You wanna go: '))
+#while userTurn == 1:
+    
+
