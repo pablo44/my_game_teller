c55518bdeca22d5348d46b351c97bc9df285a7e0
pablo44 <pawelrz@wp.pl>
2023-09-25 17:20:33 +0200

    functions- userTurn and createEnv

--- game.py
+++ game.py
@@ -1,18 +1,84 @@
 #WELCOME 
+print('********************************')
 print('Welcome to the "CASTLE GAME"')
 print('********************************')
-print('You are in the Town of Abarka \n The Town is surrounded by the mountains and placed by the river')
-name = input('Would You please introduce Yourselve, Write Your Name: ')
-print(f'So {name} You are standing at the marketplace now in the center of the Town of Abarka')
-#options
+print('\n')
+print('You are in the Town of Abarka \nThe Town is surrounded by the mountains and placed by the river')
+print('\n')
+name = input('Would You please introduce Yourself, Write Your Name: ')
+print('\n')
+print(f'So {name} You are standing at the marketplace now in the heart of Abarka')
+
+
+
+
+
+#options, two lists that will be used in options menue
 abarka = ['north', 'west', 'east','south']
-print(f'You have \n on Your {abarka[0]} side Mountains \n on Your {abarka[1]} side Valley \n on Your {abarka[2]} side Desert \n on Your {abarka[3]} side River')
-"*************************************"
-userTurn = ""
-'*********************'
-userTurn= input
-print('choose your number: \n 1 north \n 2 west \n 3 east \n 4 south')
-print(input(f'{name} choose where do You wanna go: '))
-#while userTurn == 1:
+numbers = ['1', '2', '3', '4']
+print("**************************************************************************")
+print("**************************************************************************")
+
+
+
+
+#function to get the users choice and set it into variable userTurn
+def getUserTurn():
+    userTurn = input('Write your choice which direction you choose: ')
+    if userTurn.isdigit():
+        if int(userTurn) > 0 and int(userTurn)< 4:
+            return int(userTurn)
+    return -1
+
+
+
+
+#function that creates the environment based on the users input
+def createEnv():
+    if userChoice ==1:
+        print(' \u0394, \u0394. \u0394, \u0394')
+    elif userChoice ==2:
+       print('¨~~~~~~~~~')
+    elif userChoice ==3:
+       print('...........')
+    elif userChoice ==4:
+        print('~~...~~~...~~')
+    else:
+       print('no environment')
+    
+
+
+
+# two lists merget by for loop in order to shwow the options
+print('choose the site You wanna go:')
+for x, i in zip(numbers, abarka):
+    print(x,'.', i)
+
+
+
+
+#using function getUserTurn to show the decision
+userChoice = getUserTurn()
+gameEnv = createEnv()
+if userChoice == -1:
+    print('write only a number')
+    print(createEnv())
+if userChoice == 1:
+    print('you are in the mountains')
+    print(createEnv())
+elif userChoice == 2:
+    print('you are in the Valley')
+    print(createEnv())
+elif userChoice == 3:
+    print('you are in the Desert')
+    print(createEnv())
+elif userChoice == 4:
+    print('you are at the river side')
+    print(createEnv())
+else:
+    print('sorry type again, did not understand')
+        
+            
+
     
 
